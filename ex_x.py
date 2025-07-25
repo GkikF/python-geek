8967d3bd4e7ad90af9a2b564b3af0b80ca9d4995
yannisf <yannisf@ouranos>
2025-07-25 09:18:35 +0300

    New exercise

---
+++ ex_x.py
@@ -0,0 +1,5 @@
+# Create a function that receives a string and prints "Hello, <the string>"
+
+# Read the string from the terminal (input)
+
+# If the input name is gkik respond with "Hello, Gkik", else respond with "Hello stranger"
